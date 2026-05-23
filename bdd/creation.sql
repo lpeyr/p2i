9e9853ccde51712e36ca7a2a0ae0033230e4db25
Léo Peyronnet <leo.peyronnet@outlook.fr>
2026-05-23 13:51:18 +0200

    chore: Reformat SQL Code

--- bdd/creation.sql
+++ bdd/creation.sql
@@ -1,80 +1,80 @@
-Drop Table if exists MesureIMU;
+DROP TABLE IF EXISTS MesureIMU;
 
-Drop Table if exists MesureFlexi;
+DROP TABLE IF EXISTS MesureFlexi;
 
-Drop Table if exists MesureGPS;
+DROP TABLE IF EXISTS MesureGPS;
 
-Drop Table if exists Session;
-Drop Table if exists Semelle;
+DROP TABLE IF EXISTS Session;
+DROP TABLE IF EXISTS Semelle;
 
-Drop Table if exists Utilisateur;
+DROP TABLE IF EXISTS Utilisateur;
 
-Create Table Utilisateur
+CREATE TABLE Utilisateur
 (
-    idUser INT(10) UNSIGNED auto_increment Primary key,
-    nom    varchar(50)      NOT NULL,
-    prenom varchar(50)      NOT NULL,
-    role   varchar(20)      NOT NULL,
+    idUser INT(10) UNSIGNED auto_increment PRIMARY KEY,
+    nom    VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    role   VARCHAR(20) NOT NULL,
     age    INT(10) Unsigned NOT NULL,
-    poids  INT(10) Unsigned Not NULL,
+    poids  INT(10) Unsigned NOT NULL,
     taille INT(10) Unsigned NOT NULL
 );
 
 
-Create Table Semelle
+CREATE TABLE Semelle
 (
-    idSemelle INT(10) UNSIGNED auto_increment Primary Key,
+    idSemelle INT(10) UNSIGNED auto_increment PRIMARY KEY,
     idUser    INT(10) UNSIGNED      NOT NULL,
-    devEUI    char(16)              NOT NULL,
-    side      enum ("left","right") NOT NULL,
-    Foreign Key (idUser) References Utilisateur (idUser)
+    devEUI    CHAR(16)              NOT NULL,
+    side      enum("left", "right") NOT NULL,
+    FOREIGN KEY (idUser) REFERENCES Utilisateur (idUser)
 );
 
-Create Table Session
+CREATE TABLE Session
 (
-    idSession       INT(10) UNSIGNED auto_increment Primary key,
-    dateDebut       DATETIME         Not NULL,
+    idSession       INT(10) UNSIGNED auto_increment PRIMARY KEY,
+    dateDebut       DATETIME NOT NULL,
     dateFin         DATETIME,
     semelle1        INT(10) UNSIGNED NOT NULL,
     semelle2        INT(10) UNSIGNED NOT NULL,
     step            INT(10) UNSIGNED,
-    averageStepTime float(4, 2) UNSIGNED,
-    Foreign key (semelle1) References Semelle (idSemelle),
-    Foreign Key (semelle2) References Semelle (idSemelle),
-    Constraint Check (dateFin is NUll or dateFin > dateDebut)
+    averageStepTime FLOAT(4, 2) UNSIGNED,
+    FOREIGN KEY (semelle1) REFERENCES Semelle (idSemelle),
+    FOREIGN KEY (semelle2) REFERENCES Semelle (idSemelle),
+    CONSTRAINT CHECK (dateFin IS NULL OR dateFin > dateDebut)
 );
 
-Create Table MesureGPS
+CREATE TABLE MesureGPS
 (
-    idMesure  INT(10) UNSIGNED auto_increment Primary Key NOT NULL,
+    idMesure  INT(10) UNSIGNED auto_increment PRIMARY KEY NOT NULL,
     time      TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    lattitude float,
-    longitude float,
+    lattitude FLOAT,
+    longitude FLOAT,
     idSession INT(10) UNSIGNED                            NOT NULL,
     idSemelle INT(10) UNSIGNED                            NOT NULL,
-    Foreign Key (idSession) References Session (idSession),
-    Foreign Key (idSemelle) References Semelle (idSemelle)
+    FOREIGN KEY (idSession) REFERENCES Session (idSession),
+    FOREIGN KEY (idSemelle) REFERENCES Semelle (idSemelle)
 );
 
 
-Create Table MesureFlexi
+CREATE TABLE MesureFlexi
 (
-    idMesureFlexi INT(10) UNSIGNED auto_increment Primary Key NOT NULL,
+    idMesureFlexi INT(10) UNSIGNED auto_increment PRIMARY KEY NOT NULL,
     time          TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    flexi1        bool                                        NOT NULL,
-    flexi2        bool                                        NOT NULL,
-    flexi3        bool                                        NOT NULL,
+    flexi1        bool NOT NULL,
+    flexi2        bool NOT NULL,
+    flexi3        bool NOT NULL,
     idSession     INT(10) UNSIGNED                            NOT NULL,
     idSemelle     INT(10) UNSIGNED                            NOT NULL,
-    Foreign key (idSession) References Session (idSession),
-    Foreign Key (idSemelle) References Semelle (idSemelle)
+    FOREIGN KEY (idSession) REFERENCES Session (idSession),
+    FOREIGN KEY (idSemelle) REFERENCES Semelle (idSemelle)
 );
 
-Create Table MesureIMU
+CREATE TABLE MesureIMU
 (
     id        INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     time      TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    accel     FLOAT(4, 2)      Not Null,
+    accel     FLOAT(4, 2) NOT NULL,
     idSession INT(10) UNSIGNED NOT NULL,
     idSemelle INT(10) UNSIGNED NOT NULL,
     FOREIGN KEY (idSession) REFERENCES Session (idSession),
@@ -85,8 +85,8 @@ Create Table MesureIMU
 
 
 DELETE *
-from MesureFlexi;
+FROM MesureFlexi;
 DELETE *
-from MesureGPS;
+FROM MesureGPS;
 DELETE *
-from MesureIMU;
+FROM MesureIMU;
