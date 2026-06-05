9baa6ad017dd602e2dd30f33da47446abab858b2
Léo Peyronnet <leo.peyronnet@outlook.fr>
2026-06-05 15:55:26 +0200

    feat: Add placeholder

--- semelle/components/accel-chart.tsx
+++ semelle/components/accel-chart.tsx
@@ -42,46 +42,51 @@ const mergeSeries = (seriesMap: { acc1: Acceleration[]; acc2: Acceleration[] }) 
         }));
 };
 export function AccelChart({ data }: Readonly<AccelChartProps>) {
+    const d = mergeSeries(data);
     return (
         <div style={{ width: "100%", height: 320 }}>
-            <ResponsiveContainer width="100%" height="100%">
-                <LineChart data={mergeSeries(data)}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis
-                        dataKey="timestamp"
-                        type="number"
-                        domain={["dataMin", "dataMax"]}
-                        tickFormatter={formatTime}
-                    />
-                    <YAxis />
-                    <Tooltip
-                        labelFormatter={(ts) => new Date(ts).toLocaleString("fr-FR")}
-                        contentStyle={{
-                            backgroundColor: "var(--default)",
-                            borderColor: "var(--accent)",
-                            borderRadius: 10,
-                        }}
-                    />
-                    <Legend />
-                    <Line
-                        type="monotone"
-                        dataKey="acc1"
-                        stroke="#ff696f"
-                        strokeWidth={2}
-                        dot={true}
-                        connectNulls={true}
-                    />
+            {d.length > 0 ? (
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart data={d}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis
+                            dataKey="timestamp"
+                            type="number"
+                            domain={["dataMin", "dataMax"]}
+                            tickFormatter={formatTime}
+                        />
+                        <YAxis />
+                        <Tooltip
+                            labelFormatter={(ts) => new Date(ts).toLocaleString("fr-FR")}
+                            contentStyle={{
+                                backgroundColor: "var(--default)",
+                                borderColor: "var(--accent)",
+                                borderRadius: 10,
+                            }}
+                        />
+                        <Legend />
+                        <Line
+                            type="monotone"
+                            dataKey="acc1"
+                            stroke="#ff696f"
+                            strokeWidth={2}
+                            dot={true}
+                            connectNulls={true}
+                        />
 
-                    <Line
-                        type="monotone"
-                        dataKey="acc2"
-                        stroke="#006494"
-                        strokeWidth={2}
-                        dot={true}
-                        connectNulls={true}
-                    />
-                </LineChart>
-            </ResponsiveContainer>
+                        <Line
+                            type="monotone"
+                            dataKey="acc2"
+                            stroke="#006494"
+                            strokeWidth={2}
+                            dot={true}
+                            connectNulls={true}
+                        />
+                    </LineChart>
+                </ResponsiveContainer>
+            ) : (
+                <p>Pas de données à afficher pour le moment</p>
+            )}
         </div>
     );
 }
